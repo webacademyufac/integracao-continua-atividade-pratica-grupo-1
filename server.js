6bc0c74e2fd354e90dc57dc9705a48dedf4cedf9
abimaelqm <abimaelqm@hotmail.com>
2022-06-03 19:19:06 -0500

    Configuração do server.js para deploy no heroku

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
-//Install express server
+
 const express = require('express');
 const path = require('path');
-
+const nomeApp = process.env.npm_package_name;
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/sgcmapi-grupo-1'));
+app.use(express.static(`${__dirname}/dist/${nomeApp}`));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/sgcmapi-grupo-1/'}),
-);
+app.get('/*', (req, res) => {
+res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
+});
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
